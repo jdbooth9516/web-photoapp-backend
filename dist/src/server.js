109a1743c28f0e4a84578d870838ebad8996d2c2
Jeremiah Booth <jboothwebdev@gmail.com>
2021-11-21 14:51:12 -0700

    after build has been run

--- dist/src/server.js
+++ dist/src/server.js
@@ -4,18 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handleError = void 0;
-const express = require('express');
+const express_1 = __importDefault(require("express"));
 const db_1 = __importDefault(require("../config/db"));
 const cors_1 = __importDefault(require("cors"));
-const app = express();
+const app = (0, express_1.default)();
 function handleError(error, res) {
     console.error(error.message);
     res.status(500).json({ msg: "server Error" });
 }
 exports.handleError = handleError;
 (0, db_1.default)();
-app.use(express.json());
+app.use(express_1.default.json());
 app.use((0, cors_1.default)());
+app.use('/api/orders', require('../routes/order'));
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server stated on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 //# sourceMappingURL=server.js.map
